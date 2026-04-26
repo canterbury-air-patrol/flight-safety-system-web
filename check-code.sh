1c84aac6d966f730196893e627ff17daebcf30c5
Scott Parlane <sjp@canterburyairpatrol.org>
2026-04-26 12:06:28 +1200

    Split dev and production python requirements

--- check-code.sh
+++ check-code.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -ex
 
+if [ ! -d venv ]
+then
+	python3 -m venv venv
+fi
+
 source venv/bin/activate
+
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
 
 PATHS="fss/ main/ config/ assets/"
 
